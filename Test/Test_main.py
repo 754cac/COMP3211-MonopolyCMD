015223c5a6a362eefaa5bbab6a366fddf43b801e
HO134667 <131956996+HO134667@users.noreply.github.com>
2024-11-19 03:06:31 +0800

    Add Test Folder,modify import dir

---
+++ Test/Test_main.py
@@ -0,0 +1,31 @@
+import unittest
+from unittest import mock
+import sys
+sys.path.append(r'..\monopoly_game')
+from game import Game
+import vars
+
+class TestGame(unittest.TestCase):
+
+    def setUp(self):
+        self.game = Game()
+
+    def test_new_game(self):
+        self.game.new_game = mock.Mock(return_value=True)
+        game_initialized = self.game.new_game()
+        self.assertTrue(game_initialized)
+
+    def test_load_game_state(self):
+        self.game.load_game_state = mock.Mock(return_value=True)
+        save_file_name = "test_save_file"
+        game_initialized = self.game.load_game_state(save_file_name)
+        self.assertTrue(game_initialized)
+
+    def test_play(self):
+        self.game.play = mock.Mock()
+        self.game.play()
+        self.game.play.assert_called_once()
+
+if __name__ == '__main__':
+    unittest.main()
+
